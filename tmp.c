3e12e04dd82dbca8b4cdabc804eb6c045f9f7cd8
Stick-of-WuKong <83912431+Stick-of-WuKong@users.noreply.github.com>
2022-09-04 13:01:33 +0800

    Update tmp.c

--- tmp.c
+++ tmp.c
@@ -10,7 +10,22 @@
 #<include>
 def name(){
     print('hello');
-    print('this is the renwy of github')
+    print('this is the renwy of github');
+    print("### 什么是魔术引号
+
+了解一个PHP的防御函数==》 **magic_quotes_gpc**（魔术引号开关）
+
+在mysql中，用于[转义](https://so.csdn.net/so/search?q=转义&spm=1001.2101.3001.7020)（即在字符串中的符号前加上”\”）的函数有addslashes，mysql_real_escape_string，mysql_escape_string等，还有一种情况是magic_quote_gpc，不过高版本的PHP将去除这个特性。
+
+magic_quotes_gpc函数在php中的作用是判断解析用户提交的数据，如包括有：post、get、cookie过来的数据增加转义字符“\”，以确保这些数据不会引起程序，特别是数据库语句因为特殊字符引起的污染而出现致命的错误。
+
+**单引号（’）、双引号（”）、反斜线（\）等字符都会被加上反斜线**
+
+**magic_quotes_gpc的作用：当PHP的传参中有特殊字符就会再前面加转义字符'\',来做一定的过滤**
+
+单引号和双引号内的一切都是字符串，那我们输入的东西如果不能闭合掉单引号和双引号，我们的输入就不会当作代码执行，就无法产生SQL注入，那我们该怎么办
+          
+          ")
 }
 def interfaces(){
     return 1++;
