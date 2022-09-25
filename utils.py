cb107178167b0306c51f69728347d2823a4bce03
Stick-of-WuKong <83912431+Stick-of-WuKong@users.noreply.github.com>
2022-09-25 19:58:55 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,11 @@
+'''
+Author: yhh
+Date: 2022-09-16 11:12:43
+LastEditTime: 2022-09-16 11:12:43
+LastEditors: yhh
+Description: 
+FilePath: \PythonFile\utils.py
+yhh
+'''
+def add(a,b):
+    return a+b
